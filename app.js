37a3e06ee855ef3ff2ac3065d00cb5ce3aaf3921
tecknovice <tecknovice@gmail.com>
2019-08-16 17:30:35 +0700

    add compression and helment

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
 // const flash = require('express-flash')
+
 require('./config/db')
 
 const indexRouter = require('./routes/index');
@@ -13,6 +16,7 @@ const imageRouter = require('./routes/imageRouter')
 const tagRouter = require('./routes/tagRouter')
 
 const app = express();
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +26,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(flash())
 
