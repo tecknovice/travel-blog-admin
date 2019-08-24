5d46e3670e512f3ef51ca24c2d60f997d0c453d7
tecknovice <tecknovice@gmail.com>
2019-08-24 18:57:43 +0700

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,26 @@ const dest = path.join(__dirname, 'public', 'images')
 const globby = require('globby')
 const del = require('del')
 
-Array.prototype.diff = function (a) {
-    return this.filter(function (i) { return a.indexOf(i) < 0; });
-};
+// Array.prototype.diff = function (a) {
+//     return this.filter(function (i) { return a.indexOf(i) < 0; });
+// };
+
+// async function imageclean() {
+
+//     let sourceFiles = await globby(source)
+//     sourceFiles = sourceFiles.map(file => file.match(/[^/]+$/)[0])
+//     let destFiles = await globby(dest)
+//     destFiles = destFiles.map(file => file.match(/[^/]+$/)[0])
+
+//     let deleteFiles = destFiles.diff(sourceFiles)
+//     deleteFiles = deleteFiles.map(file => path.join(dest, file))
+//     const deletedPaths = await del(deleteFiles)
+
+// }
 
 async function imageclean() {
-
-    let sourceFiles = await globby(source)
-    sourceFiles = sourceFiles.map(file => file.match(/[^/]+$/)[0])
-    let destFiles = await globby(dest)
-    destFiles = destFiles.map(file => file.match(/[^/]+$/)[0])
-
-    let deleteFiles = destFiles.diff(sourceFiles)
-    deleteFiles = deleteFiles.map(file => path.join(dest, file))
-    const deletedPaths = await del(deleteFiles)
-
+    let deleteFiles = await globby(source)
+    await del(deleteFiles)
 }
 function imageminify() {
     return gulp.src(source)
